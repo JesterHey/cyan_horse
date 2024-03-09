1b1d9ed5c5ec4de6cdadba0d59001d5d05c864a8
JesterHey <Jester_Huang@hnu.edu.cn>
2024-03-09 14:58:41 +0800

    modified:   kill_course.py

--- kill_course.py
+++ kill_course.py
@@ -12,3 +12,28 @@ for k, v in info.items():
         cnt[v['type']].append(k)
 print(cnt)
 # 按必修-选秀-专题-培训执行刷课
+must = cnt['必修']
+elective = cnt['选修']
+special = cnt['专题']
+train = cnt['培训']
+while must or elective or special or train:
+    if must:
+        course_id = must.pop()
+        print('Kill course:', course_id)
+        # 执行刷课
+        # ...
+    if elective:
+        course_id = elective.pop()
+        print('Kill course:', course_id)
+        # 执行刷课
+        # ...
+    if special:
+        course_id = special.pop()
+        print('Kill course:', course_id)
+        # 执行刷课
+        # ...
+    if train:
+        course_id = train.pop()
+        print('Kill course:', course_id)
+        # 执行刷课
+        # ...
